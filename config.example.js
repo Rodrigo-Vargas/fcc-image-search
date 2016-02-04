018b3129fe2577f8274fb8ba46f4e2a1aaf8e1c1
Rodrigo <rodrigo@rodrigo-VirtualBox.skynet>
2016-02-04 19:19:13 -0200

    Added heroku env variable to config

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.API_KEY = "API_KEY_HERE"
-config.CSI_KEY = "CSI_KEY_HERE";
+config.API_KEY = "API_KEY_HERE" || process.env.API_KEY;
+config.CSI_KEY = "CSI_KEY_HERE" || process.env.CSI_KEY;
 
 module.exports = config;
